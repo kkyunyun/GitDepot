359f1dc855445311bbcc1bac581f80df8c1d7514
KKdaYLight <773020772@qq.com>
2023-08-25 16:40:29 +0800

    哦哦

--- 1.cpppp/4.Composite_type/getline_git.cpp
+++ 1.cpppp/4.Composite_type/getline_git.cpp
@@ -4,6 +4,10 @@ int main(){
     const int ArSize =20;
     char name[ArSize];
     char dessert[ArSize];
+    cout<<"What year was your house built?"<<endl;
+    int year;
+    cin>>year;
+    cin.get();//cin读取到输入的内容时会把换行符留在队列中需要清除该换行符以便后续读取正常运行
     cout<<"Enter your name:"<<endl;
     cin.get(name,ArSize).get();//get()用来处理换行符，为读取下一行输入准备
     cout<<"enter your favorite dessert"<<endl;
